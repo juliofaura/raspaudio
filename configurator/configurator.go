4d822d46d1f80a21f4432c6cd5bc48a03f76f13c
juliofaura <faura@alum.mit.edu>
2020-05-12 11:13:37 +0200

    Added windows configurator

--- configurator/configurator.go
+++ configurator/configurator.go
@@ -3,23 +3,32 @@ package main
 import (
 	//"bufio"
 	"fmt"
-	//"strings"
-	"github.com/juliofaura/raspaudio/netUtils"
+	"log"
+	"os/exec"
+	"runtime"
 )
+
+func openbrowser(url string) {
+	var err error
+
+	switch runtime.GOOS {
+	case "linux":
+		err = exec.Command("xdg-open", url).Start()
+	case "windows":
+		err = exec.Command("rundll32", "url.dll,FileProtocolHandler", url).Start()
+	case "darwin":
+		err = exec.Command("open", url).Start()
+	default:
+		err = fmt.Errorf("unsupported platform")
+	}
+	if err != nil {
+		log.Fatal(err)
+	}
+}
 
 ///////////////////////////////////////////////////
 // Main function
 ///////////////////////////////////////////////////
 func main() {
-
-	fmt.Println("Finding my own IPs")
-
-	fmt.Println("Outbound IP:", netUtils.GetOutboundIP().String())
-
-	IPs := netUtils.ResolveHostIP()
-
-	for _, v := range IPs {
-		fmt.Printf("IP is %v (mask is %v)\n", v.IP, v.Mask)
-	}
-
+	openbrowser("http://192.168.1.19:8020")
 }
